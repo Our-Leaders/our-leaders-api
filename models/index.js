bd303021360f99369a2b1b5c9521612d106761e9
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-11-06 15:15:30 +0100

    feature: add data models

---
+++ models/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by bolorundurowb on 06/11/2019
+ */
+
+const mongoose = require('mongoose');
+const Config = require('./../config/Config');
+const Logger = require('./../config/Logger');
+
+// import models here
+
+const db = {};
+mongoose.connect(Config.database, {
+  useNewUrlParser: true
+}).then(() => Logger.log('Database set up successfully.'));
+
+// inject models into return object here
+
+module.exports = db;
