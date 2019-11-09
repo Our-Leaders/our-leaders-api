8ee19a621219f90cd5308a8efe5d03264b1dcb45
saint-lawal <abiso.lawal@gmail.com>
2019-11-09 22:37:27 +0100

    #12730266 | Add politician model to index

--- models/index.js
+++ models/index.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const Config = require('./../config/Config');
 const Logger = require('./../config/Logger');
 const PoliticalParty = require('./PoliticalParty');
+const Politician = require('./Politician');
 const User = require('./User');
 
 // import models here
@@ -17,6 +18,7 @@ mongoose.connect(Config.database, {
 
 // inject models into return object here
 db.PoliticalParty = PoliticalParty;
+db.Politician = Politician;
 db.User = User;
 
 module.exports = db;
