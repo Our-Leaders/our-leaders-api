ef667fc9f6adaf3cc6887d7c4b0ea3f9e470d890
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-11-09 19:07:44 +0100

    feature: add the model to the index

--- models/index.js
+++ models/index.js
@@ -5,6 +5,7 @@
 const mongoose = require('mongoose');
 const Config = require('./../config/Config');
 const Logger = require('./../config/Logger');
+const PoliticalParty = require('./PoliticalParty');
 
 // import models here
 
@@ -14,5 +15,6 @@ mongoose.connect(Config.database, {
 }).then(() => Logger.log('Database set up successfully.'));
 
 // inject models into return object here
+db.PoliticalParty = PoliticalParty;
 
 module.exports = db;
