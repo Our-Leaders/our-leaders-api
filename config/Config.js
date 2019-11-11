9fc0c116e508b61da1d18a6c4dee009d827872f5
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-11-11 21:29:48 +0100

    chore: remove redundant facebook config

--- config/Config.js
+++ config/Config.js
@@ -22,9 +22,5 @@ module.exports = {
   },
   google: {
     clientId: process.env.GOOGLE_CLIENT_ID
-  },
-  facebook: {
-    clientId: process.env.FACEBOOK_CLIENT_ID,
-    secret: process.env.FACEBOOK_CLIENT_SECRET
   }
 };
