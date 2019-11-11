480dea8eb782ee3d7d9ef19c9cba6ea5e457b717
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-11-11 17:48:06 +0100

    feature: add output formatter implementation

--- utils/OutputFormatters.js
+++ utils/OutputFormatters.js
@@ -4,7 +4,23 @@
 
 class OutputFormatters {
   static formatUser(user) {
+    if (!user) {
+      return {};
+    }
 
+    return {
+      id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      gender: user.gender,
+      phoneNumber: user.phoneNumber,
+      email: user.email,
+      ageRange: user.ageRange,
+      isEmailVerified: user.isEmailVerified,
+      isPhoneVerified: user.isPhoneVerified,
+      role: user.role,
+      joinedAt: user.createdAt
+    };
   }
 }
 
