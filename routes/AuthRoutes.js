aae269936463b73f0889e3049803f957de25343c
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-11-11 13:31:52 +0100

    feature: set routing up

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -2,9 +2,13 @@
  * Created by bolorundurowb on 11/11/2019
  */
 
+const AuthCtrl = require('./../controllers/Auth');
+const AuthValidators = require('../validators/AuthValidators');
+
 class AuthRoutes {
   static route(router) {
-
+    router.route('/v1/auth/register')
+      .post(AuthValidators.validateSignUp, AuthCtrl.signUp);
   }
 }
 
