9b2933d19ca6b9824d56b9447999ff098ca80222
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-11-11 13:46:37 +0100

    feature: add the validator details

--- validators/AuthValidators.js
+++ validators/AuthValidators.js
@@ -3,8 +3,30 @@
  */
 
 class AuthValidators {
-  static validateSignUp(req, res) {
+  static validateSignUp(req, res, next) {
+    const body = req.body;
+    let message = '';
 
+    // check for an email address
+    if (body.email) {
+      if (!body.password) {
+        message = 'A password is required for email sign up.';
+      }
+    }
+    // check for google or facebook id
+    else {
+      if (!(body.googleId || body.facebookId)) {
+        message = 'An id is required for social sign up.';
+      }
+    }
+
+    if (message) {
+      return res.status(400).send({
+        message
+      });
+    } else {
+      next();
+    }
   }
 }
 
