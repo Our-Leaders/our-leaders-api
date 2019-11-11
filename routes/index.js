fde359550f2c35e690b99cdddd5e702cf5157e16
chinezenwosu <chineze.nwosu@andela.com>
2019-11-11 23:40:36 +0100

    create dummy routes for politicians

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@
  * Created by bolorundurowb on 06/11/2019
  */
 
+const politicians = require('./politicians');
+
 module.exports = (router) => {
   router.get('/', function (req, res) {
     res.send('Home page works');
   });
+
+  router.use('/politicians', politicians)
 
   return router;
 };
