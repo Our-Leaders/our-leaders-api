ed10d1c62decab013e188df9adf32a972c407c7f
chinezenwosu <chineze.nwosu@andela.com>
2019-11-13 14:57:36 +0100

    serialize retrieved politicians

--- controllers/Politician.js
+++ controllers/Politician.js
@@ -1,11 +1,15 @@
 const db = require('./../models');
+const OutputFormatters = require('./../utils/OutputFormatters');
 
 class Politician {
   static async find(req, res, next) {
     try {
       const politicians = await db.Politician.find();
+      const serializedPolitician = politicians.map(politician => {
+        return OutputFormatters.formatPolitician(politician);
+      })
       res.status(200).send({
-        politicians
+        politicians: serializedPolitician
       });
     } catch (error) {
       next(new ErrorHandler(500, error.message));
