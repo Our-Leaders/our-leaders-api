497383e6745fb5809b119a9091b62e165a848aab
chinezenwosu <chineze.nwosu@andela.com>
2019-11-13 16:18:40 +0100

    implement politicians query by different params

--- controllers/Politician.js
+++ controllers/Politician.js
@@ -1,10 +1,46 @@
 const db = require('./../models');
 const OutputFormatters = require('./../utils/OutputFormatters');
+const { ErrorHandler } = require('./../utils/errorUtil');
 
 class Politician {
   static async find(req, res, next) {
+    const query = req.query;
+    let findByQuery = {};
+    let orQuery = [];
+
+    if (query.name) {
+      orQuery.push({ name: { $regex: query.name, $options: 'i' } });
+    }
+
+    if (query.status) {
+      orQuery.push({ status: { $regex: query.status, $options: 'i' } });
+    }
+
+    if (query.state) {
+      orQuery.push({ state: { $regex: query.state, $options: 'i' } });
+    }
+
+    if (query.politicalParty) {
+      orQuery.push({ politicalParty: { $regex: query.politicalParty, $options: 'i' } });
+    }
+
+    if (query.politicalPosition) {
+      orQuery.push({
+        politicalBackground: {
+          $elemMatch: {
+            inOffice: true,
+            position: { $regex: query.politicalPosition, $options: 'i' }
+          }
+        }
+      });
+    }
+
+    if (orQuery.length) {
+      findByQuery = { $or: orQuery };
+    }
+
     try {
-      const politicians = await db.Politician.find();
+      const politicians = await db.Politician.find(findByQuery);
       const serializedPolitician = politicians.map(politician => {
         return OutputFormatters.formatPolitician(politician);
       })
