85d188a6652d46ac4098af675616540660b58388
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-11-14 14:35:23 +0100

    feature: add authentication

--- routes/SubscriptionRoutes.js
+++ routes/SubscriptionRoutes.js
@@ -2,12 +2,14 @@
  * Created by bolorundurowb on 14/11/2019
  */
 
+const AuthMiddleware = require('./../middleware/AuthenticationMiddleware');
 const SubscriptionCtrl = require('./../controllers/Subscriptions');
 const SubscriptionValidators = require('./../validators/SubscriptionValidators');
 
 module.exports = (router) => {
   router.route('/subscriptions')
     .post(
+      AuthMiddleware.authenticate,
       SubscriptionValidators.validateCreation,
       SubscriptionCtrl.addSubscription
     );
