7f2b4efec8fc69c01fc0691e59c33d6297c15228
chinezenwosu <chineze.nwosu@andela.com>
2019-11-14 01:12:27 +0100

    implement create in controller

--- controllers/Politician.js
+++ controllers/Politician.js
@@ -3,6 +3,19 @@ const OutputFormatters = require('./../utils/OutputFormatters');
 const { ErrorHandler } = require('./../utils/errorUtil');
 
 class Politician {
+  static async create(req, res, next) {
+    try {
+      const politician = new db.Politician(req.body);
+      await politician.save();
+
+      res.status(201).send({
+        politician: OutputFormatters.formatPolitician(politician)
+      });
+    } catch (error) {
+      next(new ErrorHandler(500, error.message));
+    }
+  }
+
   static async find(req, res, next) {
     const query = req.query;
     let findByQuery = {};
@@ -41,11 +54,11 @@ class Politician {
 
     try {
       const politicians = await db.Politician.find(findByQuery);
-      const serializedPolitician = politicians.map(politician => {
+      const serializedPoliticians = politicians.map(politician => {
         return OutputFormatters.formatPolitician(politician);
       })
       res.status(200).send({
-        politicians: serializedPolitician
+        politicians: serializedPoliticians
       });
     } catch (error) {
       next(new ErrorHandler(500, error.message));
@@ -61,7 +74,7 @@ class Politician {
 
       if (politician) {
         res.status(200).send({
-          politicians: OutputFormatters.formatPolitician(politician)
+          politician: OutputFormatters.formatPolitician(politician)
         });
       } else {
         next(new ErrorHandler(404, 'Politician doesn\'t exist'));
