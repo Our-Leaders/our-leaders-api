3d9e8a3ce11dce7bced80bdfeabc46b9d4b3de84
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-11-28 11:59:15 +0100

    ft: add config vars as well as logging for the feeds job

--- cron/FeedsJob.js
+++ cron/FeedsJob.js
@@ -2,9 +2,17 @@
  * Created by bolorundurowb on 28/11/2019
  */
 
-class FeedsJob {
-  static run() {
+const Config = require('./../config/Config');
+const Logger = require('./../config/Logger');
 
+class FeedsJob {
+  static async run() {
+    try {
+
+    } catch (err) {
+      Logger.error(err);
+      Logger.error('An error occurred when refreshing feeds.')
+    }
   }
 }
 
