b119f8a14110493fa3277e59ce744fb94280d2c2
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-11-28 11:47:48 +0100

    ft: add cron scheduler implementation

---
+++ jobs/CronScheduler.js
@@ -0,0 +1,17 @@
+/**
+ * Created by bolorundurowb on 28/11/2019
+ */
+
+const nodeCron = require('node-cron');
+const FeedsJob = require('./FeedsJob');
+const Logger = require('./../config/Logger');
+
+class CronScheduler {
+  static startJobs() {
+    nodeCron.schedule('0 */2 * * *', FeedsJob.run);
+
+    Logger.log('Cron jobs started successfully.');
+  }
+}
+
+module.exports = CronScheduler;
---
+++ jobs/FeedsJob.js
@@ -0,0 +1,11 @@
+/**
+ * Created by bolorundurowb on 28/11/2019
+ */
+
+class FeedsJob {
+  static run() {
+
+  }
+}
+
+module.exports = FeedsJob;
