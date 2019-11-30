095ce407bf366ad13c0bc7b784fa9c6b2d8399da
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-11-30 22:58:28 +0100

    ft: add feed routes

---
+++ routes/FeedRoutes.js
@@ -0,0 +1,14 @@
+/**
+ * Created by bolorundurowb on 14/11/2019
+ */
+
+const AuthMiddleware = require('./../middleware/AuthenticationMiddleware');
+const FeedsCtrl = require('./../controllers/Feeds');
+
+module.exports = (router) => {
+  router.route('/feeds')
+    .post(
+      AuthMiddleware.authenticate,
+      FeedsCtrl.getFeeds
+    );
+};
