67c0793fba0110b84bf62e422f3f7ebfa3081385
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-12-03 18:09:09 +0100

    ft: add email sending class

---
+++ communications/Email.js
@@ -0,0 +1,33 @@
+/**
+ * Created by bolorundurowb on 03/12/2019
+ */
+
+const Mailgun = require('mailgun.js');
+
+const Config = require('./../config/Config');
+const Logger = require('./../config/Logger');
+
+const mg = Mailgun.client({
+  username: 'api',
+  key: Config.mailGun.apiKey
+});
+
+/**
+ * Hold all email configuration and delivery logic
+ */
+class Email {
+  /**
+   * Sends an email using MailGun
+   * @param {Object} payload
+   */
+  static async send(payload) {
+    try {
+      await mg.messages.create(Config.mailGun.domain, payload);
+      Logger.log('The email(s) was/were sent successfully.');
+    } catch (err) {
+      Logger.error(err);
+    }
+  }
+}
+
+module.exports = Email;
