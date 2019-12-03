615babe1d44b528abc76cf8060388038360cbacd
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-12-03 20:43:38 +0100

    ft: add email subscription retrieval logic

--- utils/EmailUtil.js
+++ utils/EmailUtil.js
@@ -9,11 +9,22 @@ const path = require('path');
 const Config = require('./../config/Config');
 
 class EmailUtil {
+  static getSubscriptionEmail(email, firstName, feeds) {
+    const templateName = 'subscriptionFeed';
+
+    return {
+      from: 'Our Leaders <no-reply@our-leaders.org>',
+      to: [email],
+      subject: `Feeds from your subscriptions`,
+      html: EmailUtil.generateHtml(templateName, {firstName, feeds})
+    };
+  }
+
   static generateHtml(templateName, payload) {
-    const baseEmailTemplateString = fs.readFileSync(path.join(path.dirname(__dirname), 'templates', 'baseEmailTemplate.hbs'), 'utf8');
+    const baseEmailTemplateString = fs.readFileSync(path.join(path.dirname(__dirname), 'communications', 'templates', 'baseEmailTemplate.hbs'), 'utf8');
     const baseEmailTemplate = handlebars.compile(baseEmailTemplateString);
 
-    const templateString = fs.readFileSync(path.join(path.dirname(__dirname), 'templates', `${templateName}.hbs`), 'utf8');
+    const templateString = fs.readFileSync(path.join(path.dirname(__dirname), 'communications', 'templates', `${templateName}.hbs`), 'utf8');
     const template = handlebars.compile(templateString);
 
     const body = template(payload);
