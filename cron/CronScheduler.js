63f1b204c879568e4bbb526309e4cb25d5d1221e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-12-04 13:13:34 +0100

    ft: add options for the scheduled tasks

--- cron/CronScheduler.js
+++ cron/CronScheduler.js
@@ -9,17 +9,22 @@ const Logger = require('./../config/Logger');
 
 class CronScheduler {
   static startJobs() {
+    const cronOptions = {
+      scheduled: true,
+      timezone: 'Africa/Lagos'
+    };
+
     // check feeds every two hours
-    nodeCron.schedule('0 */2 * * *', FeedsJob.run);
+    nodeCron.schedule('0 */2 * * *', FeedsJob.run, cronOptions);
 
     // send daily subscriptions at 12 PM
-    nodeCron.schedule('0 */12 * * *', SubscriptionJobs.dispatchDailyFeeds);
+    nodeCron.schedule('0 */12 * * *', SubscriptionJobs.dispatchDailyFeeds, cronOptions);
 
     // send weekly subscriptions at 12 PM every Sat
-    nodeCron.schedule('0 0 12 * * SAT', SubscriptionJobs.dispatchWeeklyFeeds);
+    nodeCron.schedule('0 0 12 * * SAT', SubscriptionJobs.dispatchWeeklyFeeds, cronOptions);
 
     // send monthly subscriptions at 12 PM on the last month day
-    nodeCron.schedule('0 12 29 * *', SubscriptionJobs.dispatchMonthlyFeeds);
+    nodeCron.schedule('0 12 29 * *', SubscriptionJobs.dispatchMonthlyFeeds, cronOptions);
 
     Logger.log('Cron jobs started successfully.');
   }
