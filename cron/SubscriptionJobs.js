57797eefc35d2d83140dbbe3c894d03be38d887d
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-12-04 15:01:58 +0100

    ft: complete notifications dispatch logic

--- cron/SubscriptionJobs.js
+++ cron/SubscriptionJobs.js
@@ -12,53 +12,7 @@ const Logger = require('./../config/Logger');
 class SubscriptionJobs {
   static async dispatchDailyFeeds() {
     try {
-      const dailySubscriptions = await db.Subscription
-        .find({frequency: 'daily'});
-
-      // if there are no subscriptions stop
-      if (dailySubscriptions.length === 0) {
-        return;
-      }
-
-      // define query variables
-      const today = new Date();
-      const lowerBound = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1, 12);
-      const upperBound = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 12);
-
-      // group subscriptions by user id
-      const subscriptionsByUser = _.groupBy(dailySubscriptions, 'user');
-
-      // iterate over the user subscriptions and get feeds
-      for (let userId in subscriptionsByUser) {
-        if (subscriptionsByUser.hasOwnProperty(userId)) {
-          const user = await db.User
-            .findById(userId)
-            .select('firstName email');
-
-          if (!user) {
-            continue;
-          }
-
-          const politicianIds = subscriptionsByUser[userId].map(({politician}) => politician);
-
-          // check for feeds with the politician ids
-          const feeds = await FeedUtil.queryFeeds(politicianIds, {
-            createdAt: {
-              $lte: upperBound,
-              $gte: lowerBound
-            }
-          });
-
-          // if there are no fields then skip
-          if (feeds.length === 0) {
-            continue;
-          }
-
-          // send feed summary to user
-          const payload = EmailUtil.getSubscriptionEmail(user.email, user.firstName, feeds);
-          await Email.send(payload);
-        }
-      }
+      await SubscriptionJobs.dispatchFeeds('daily', 1);
     } catch (err) {
       Logger.error(err);
       Logger.error('An error occurred when dispatching daily feeds.')
@@ -66,11 +20,71 @@ class SubscriptionJobs {
   }
 
   static async dispatchWeeklyFeeds() {
-
+    try {
+      await SubscriptionJobs.dispatchFeeds('weekly', 7);
+    } catch (err) {
+      Logger.error(err);
+      Logger.error('An error occurred when dispatching weekly feeds.')
+    }
   }
 
   static async dispatchMonthlyFeeds() {
+    try {
+      await SubscriptionJobs.dispatchFeeds('monthly', 30);
+    } catch (err) {
+      Logger.error(err);
+      Logger.error('An error occurred when dispatching monthly feeds.')
+    }
+  }
 
+  static async dispatchFeeds(frequency, lengthOfDays) {
+    const subscriptions = await db.Subscription
+      .find({frequency});
+
+    // if there are no subscriptions stop
+    if (subscriptions.length === 0) {
+      return;
+    }
+
+    // define query variables
+    const today = new Date();
+    const lowerBound = new Date(today.getFullYear(), today.getMonth(), today.getDate() - lengthOfDays, 12);
+    const upperBound = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 12);
+
+    // group subscriptions by user id
+    const subscriptionsByUser = _.groupBy(subscriptions, 'user');
+
+    // iterate over the user subscriptions and get feeds
+    for (let userId in subscriptionsByUser) {
+      if (subscriptionsByUser.hasOwnProperty(userId)) {
+        const user = await db.User
+          .findById(userId)
+          .select('firstName email');
+
+        if (!user) {
+          continue;
+        }
+
+        const politicianIds = subscriptionsByUser[userId].map(({politician}) => politician);
+
+        // check for feeds with the politician ids
+        const feeds = await FeedUtil.queryFeeds(politicianIds, {
+          createdAt: {
+            $lte: upperBound,
+            $gte: lowerBound
+          }
+        });
+
+        // if there are no fields then skip
+        if (feeds.length === 0) {
+          continue;
+        }
+
+        // send feed summary to user
+        const payload = EmailUtil.getSubscriptionEmail(user.email, user.firstName, feeds);
+        await Email.send(payload);
+      }
+    }
   }
 }
 
