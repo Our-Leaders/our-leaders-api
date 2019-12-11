03a5dae17d282658633aac956ea7cdd959dd7435
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-12-11 11:42:45 +0100

    ft: complete endpoint implementations

--- controllers/Users.js
+++ controllers/Users.js
@@ -3,14 +3,50 @@
  */
 
 const db = require('./../models');
+const {ErrorHandler} = require('../utils/errorUtil');
+const OutputFormatters = require('./../utils/OutputFormatters');
 
 class Users {
-  static async blockUser(req, res) {
+  static async blockUser(req, res, next) {
+    const {userId} = req.params;
 
+    try {
+      const user = await db.User.findById(userId);
+
+      if (!user) {
+        return next(new ErrorHandler(404, 'A user with the provided id does not exist.'));
+      }
+
+      user.isBlocked = true;
+      await user.save();
+
+      res.status(200).send({
+        user: OutputFormatters.formatUser(user)
+      });
+    } catch (err) {
+      next(new ErrorHandler(500, 'An error occurred.'));
+    }
   }
 
-  static async unblockUser(req, res) {
+  static async unblockUser(req, res, next) {
+    const {userId} = req.params;
 
+    try {
+      const user = await db.User.findById(userId);
+
+      if (!user) {
+        return next(new ErrorHandler(404, 'A user with the provided id does not exist.'));
+      }
+
+      user.isBlocked = false;
+      await user.save();
+
+      res.status(200).send({
+        user: OutputFormatters.formatUser(user)
+      });
+    } catch (err) {
+      next(new ErrorHandler(500, 'An error occurred.'));
+    }
   }
 }
 
