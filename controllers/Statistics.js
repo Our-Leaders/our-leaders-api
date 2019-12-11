f849ae0a2861a98fc2fa7bafb0ad655856ed7a8a
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-12-11 12:08:45 +0100

    ft: complete the statistic controller implemetatopn

--- controllers/Statistics.js
+++ controllers/Statistics.js
@@ -8,7 +8,16 @@ const {ErrorHandler} = require('../utils/ErrorUtil');
 class Statistics {
   static async getStats(req, res, next) {
     try {
+      const response = {
+        parties: await db.PoliticalParty.count({}),
+        admins: await db.User.count({$or: [{role: 'admin'}, {role: 'superadmin'}]}),
+        currentLeaders: await db.Politician.count({status: 'current'}),
+        upcomingLeaders: await db.Politician.count({status: 'upcoming'})
+      };
 
+      res.status(200).send({
+        statistics: response
+      });
     } catch (error) {
       next(new ErrorHandler(500, error.message));
     }
