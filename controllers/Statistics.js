6e9d3d87ed98beac6990b0403ea645a42994e2b0
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-12-11 12:19:01 +0100

    ft: update the stats controller to support displaying signups

--- controllers/Statistics.js
+++ controllers/Statistics.js
@@ -15,6 +15,31 @@ class Statistics {
         upcomingLeaders: await db.Politician.count({status: 'upcoming'})
       };
 
+      // get the list of sign ups for the day
+      const dayStart = new Date();
+      dayStart.setHours(0, 0, 0, 0);
+
+      const dayEnd = new Date();
+      dayEnd.setHours(23, 59, 59, 999);
+
+      const newSignUps = await db.User
+        .find({
+          createdAt: {
+            $gte: dayStart,
+            $lte: dayEnd
+          }
+        })
+        .select('email createdAt');
+
+      // format the response
+      response.signUps = newSignUps.map(user => {
+        return {
+          id: user._id,
+          email: user.email,
+          createdAt: user.createdAt
+        };
+      });
+
       res.status(200).send({
         statistics: response
       });
