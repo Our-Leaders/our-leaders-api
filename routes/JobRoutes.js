8c764475b0a595a43a38d2ee2163af37a9f40b64
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-12-12 12:05:50 +0100

    ft: add the job routes

---
+++ routes/JobRoutes.js
@@ -0,0 +1,16 @@
+/**
+ * Created by bolorundurowb on 14/11/2019
+ */
+
+const AuthMiddleware = require('./../middleware/AuthenticationMiddleware');
+const JobValidators = require('./../validators/JobValidators');
+const JobsCtrl = require('./../controllers/Jobs');
+
+module.exports = (router) => {
+  router.route('/jobs')
+    .post(
+      AuthMiddleware.authenticate,
+      JobValidators.validateCreation,
+      JobsCtrl.addJobListing
+    );
+};
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@
 
 const AuthRoutes = require('./AuthRoutes');
 const FeedRoutes = require('./FeedRoutes');
+const JobRoutes = require('./JobRoutes');
 const politicianRoutes = require('./politicianRoutes');
 const StatisticRoutes = require('./StatisticRoutes');
 const SubscriptionRoutes = require('./SubscriptionRoutes');
@@ -12,6 +13,7 @@ const UserRoutes = require('./UserRoutes');
 module.exports = (router) => {
   AuthRoutes(router);
   FeedRoutes(router);
+  JobRoutes(router);
   politicianRoutes(router);
   StatisticRoutes(router);
   SubscriptionRoutes(router);
