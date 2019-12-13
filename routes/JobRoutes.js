50e5eb370071a5976b5b91884e8f7006d38dc156
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-12-13 23:35:44 +0100

    ft: complete routing

--- routes/JobRoutes.js
+++ routes/JobRoutes.js
@@ -11,7 +11,32 @@ module.exports = (router) => {
     .post(
       AuthMiddleware.authenticate,
       AuthMiddleware.isAdmin,
+      AuthMiddleware.hasPermission({property: 'jobs', action: 'create'}),
       JobValidators.validateCreation,
       JobsCtrl.addJobListing
+    );
+
+  router.route('/jobs/:jobId')
+    .put(
+      AuthMiddleware.authenticate,
+      AuthMiddleware.isAdmin,
+      AuthMiddleware.hasPermission({property: 'jobs', action: 'update'}),
+      JobsCtrl.updateJobListing
+    );
+
+  router.route('/jobs/:jobId/archive')
+    .put(
+      AuthMiddleware.authenticate,
+      AuthMiddleware.isAdmin,
+      AuthMiddleware.hasPermission({property: 'jobs', action: 'update'}),
+      JobsCtrl.archiveJobListing
+    );
+
+  router.route('/jobs/:jobId/unarchive')
+    .put(
+      AuthMiddleware.authenticate,
+      AuthMiddleware.isAdmin,
+      AuthMiddleware.hasPermission({property: 'jobs', action: 'update'}),
+      JobsCtrl.unarchiveJobListing
     );
 };
