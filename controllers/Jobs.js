23574407af8727a6f23431a4dfcd45fc32feca10
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-12-13 23:27:39 +0100

    ft: add controller action implementation

--- controllers/Jobs.js
+++ controllers/Jobs.js
@@ -17,6 +17,33 @@ class Jobs {
       next(new ErrorHandler(500, error.message));
     }
   }
+
+  static async updateJobListing(req, res, next) {
+    const {body, params} = req;
+    const {jobId} = params;
+
+    try {
+      const job = await db.Job.findById(jobId);
+
+      if (!job) {
+        return next(new ErrorHandler(404, 'A job listing with the provided id does not.'));
+      }
+
+      ['title', 'description', 'location', 'category'].forEach(prop => {
+        if (body[prop]) {
+          job[prop] = body[prop];
+        }
+      });
+
+      await job.save();
+
+      res.status(200).send({
+        job: OutputFormatters.formatJob(job)
+      });
+    } catch (error) {
+      next(new ErrorHandler(500, error.message));
+    }
+  }
 }
 
 module.exports = Jobs;
