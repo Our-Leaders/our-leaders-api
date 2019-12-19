17f3e3acc501e21288c4c5849dcefaba892d3e6b
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-12-19 18:29:02 +0100

    ft: add the admins controller scaffold

---
+++ controllers/Admins.js
@@ -0,0 +1,15 @@
+/**
+ * Created by bolorundurowb on 19/12/2019
+ */
+
+class Admins {
+  static async createAdmin(req, res, next) {
+    try {
+
+    } catch (err) {
+
+    }
+  }
+}
+
+module.exports = Admins;
