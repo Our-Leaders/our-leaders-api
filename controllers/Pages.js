269ff911f64aed5a50e6c87aa50f0630bed85e2e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2019-12-28 00:20:11 +0100

    ft: add the controller action logic

--- controllers/Pages.js
+++ controllers/Pages.js
@@ -25,6 +25,36 @@ class Pages {
       next(new ErrorHandler(500, error.message));
     }
   }
+
+  static async updatePages(req, res, next) {
+    const {body} = req;
+
+    try {
+      const page = await db.Page.findOne({});
+
+      if (!page) {
+        next(new ErrorHandler(404, 'Web pages info was not found.'));
+      }
+
+      ['aboutUs', 'contact', 'socials'].forEach(prop => {
+        if (body[prop]) {
+          page[prop] = prop;
+        }
+      });
+
+      await page.save();
+
+      res.status(200).send({
+        page: {
+          aboutUs: page.aboutUs,
+          contact: page.contact,
+          socials: page.socials
+        }
+      });
+    } catch (error) {
+      next(new ErrorHandler(500, error.message));
+    }
+  }
 }
 
 module.exports = Pages;
