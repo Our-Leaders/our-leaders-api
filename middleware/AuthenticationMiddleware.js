47d197c0721abc54c41da5ce0115d4b3e39b32db
Ralph Olutola <oreofe.olutola@gmail.com>
2019-12-31 18:49:41 +0100

    Add image upload to create politician route

--- middleware/AuthenticationMiddleware.js
+++ middleware/AuthenticationMiddleware.js
@@ -11,60 +11,50 @@ class AuthenticationMiddleware {
   static async authenticate(req, res, next) {
     const token = req.headers['x-access-token'] || req.headers['authorization'];
     if (!token) {
-      return res.status(401).send({
-        message: 'An authentication token in the header is required.'
-      });
+      return next(ErrorHandler(401, 'An authentication token in the header is required.'));
     }
 
     try {
       const decoded = await jwt.verify(token, Config.secret);
       if (!decoded) {
-        return res.status(401).send({
-          message: 'The token provided was invalid or expired. Please login again.'
-        });
+        return next(ErrorHandler(401, 'The token provided was invalid or expired. Please login again.'));
       } else {
         req.user = decoded;
         next();
       }
     } catch (err) {
       Logger.error(err);
-      res.status(500).send({
-        message: err.message || JSON.stringify(err)
-      });
+      return next(ErrorHandler(500, err.message || JSON.stringify(err)));
     }
   }
 
-  static async isAdmin(req, res, next) {
+  static isAdmin(req, res, next) {
     if (req.user.role === 'superadmin' || req.user.role === 'admin') {
       next();
     } else {
-      res.status(403).send({
-        message: 'You lack necessary permissions to carry out this action.'
-      });
+      next(ErrorHandler(403, 'You lack necessary permissions to carry out this action.'));
     }
   }
 
-  static async isSuperAdmin(req, res, next) {
+  static isSuperAdmin(req, res, next) {
     if (req.user.role === 'superadmin') {
       next();
     } else {
-      res.status(403).send({
-        message: 'You lack necessary permissions to carry out this action.'
-      });
+      next(ErrorHandler(403, 'You lack necessary permissions to carry out this action.'));
     }
   }
 
   // I feel we should have a more robust check for permissions for this
   static hasPermission({ property, action }) {
-    return async function (req, res, next) {
+    return function (req, res, next) {
       const { user } = req;
       const { permissions = {} } = user;
       const propertyPermissionObject = permissions[property];
 
       if (propertyPermissionObject[action]) {
-        next();
+        return next();
       } else {
-        next(new ErrorHandler(403, 'You do not have sufficient privileges to carry out this action.'));
+        return next(new ErrorHandler(403, 'You do not have sufficient privileges to carry out this action.'));
       }
     };
   }
