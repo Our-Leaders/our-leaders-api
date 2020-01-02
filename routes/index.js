10b97d72d492e97fb9c0deb94e8974d59703851e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-01-02 13:42:22 +0100

    ft: fix routes

--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,28 @@
  * Created by bolorundurowb on 06/11/2019
  */
 
+const AdminRoutes = require('./AdminRoutes');
 const AuthRoutes = require('./AuthRoutes');
 const FeedRoutes = require('./FeedRoutes');
 const JobRoutes = require('./JobRoutes');
-const politicalPartyRoutes = require('./politicalPartyRoutes');
-const politicianRoutes = require('./politicianRoutes');
+const PageRoutes = require('./PageRoutes');
+const PoliticalPartyRoutes = require('./PoliticalPartyRoutes');
+const PoliticianRoutes = require('./PoliticianRoutes');
 const StatisticRoutes = require('./StatisticRoutes');
 const SubscriptionRoutes = require('./SubscriptionRoutes');
 const UserRoutes = require('./UserRoutes');
-const AdminRoutes = require('./AdminRoutes');
-const PageRoutes = require('./PageRoutes');
 
 module.exports = (router) => {
+  AdminRoutes(router);
   AuthRoutes(router);
   FeedRoutes(router);
   JobRoutes(router);
-  politicalPartyRoutes(router);
-  politicianRoutes(router);
+  PageRoutes(router);
+  PoliticalPartyRoutes(router);
+  PoliticianRoutes(router);
   StatisticRoutes(router);
   SubscriptionRoutes(router);
   UserRoutes(router);
-  AdminRoutes(router);
-  PageRoutes(router);
 
   return router;
 };
