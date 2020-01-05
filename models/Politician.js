a7361197e0fa7c7da4fe67636bafd4a4cce5f012
chinezenwosu <chineze.nwosu@andela.com>
2020-01-05 23:32:14 +0100

    add numberOfViews to Politician model

--- models/Politician.js
+++ models/Politician.js
@@ -43,6 +43,10 @@ const PoliticianModel = mongoose.model('Politician', new Schema({
     type: String,
     enum: ['upcoming', 'current', 'past']
   },
+  numberOfViews: {
+    type: Number,
+    default: 0
+  },
   vote: {
     down: {
       type: Number,
