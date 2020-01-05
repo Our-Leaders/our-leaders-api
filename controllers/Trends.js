343807e4dc6312eb5f923071c903b213ce4ea051
chinezenwosu <chineze.nwosu@andela.com>
2020-01-05 23:30:38 +0100

    implement trend create in controller

---
+++ controllers/Trends.js
@@ -0,0 +1,34 @@
+const db = require('./../models');
+const OutputFormatters = require('./../utils/OutputFormatters');
+const {ErrorHandler} = require('../utils/ErrorUtil');
+
+class Trends {
+  static async create(req, res, next) {
+    const {body} = req;
+
+    try {
+      let totalTrends = await db.Trend.countDocuments();
+      let trend = await db.Trend.findOne({
+        politician: body.politicianId
+      });
+
+      // if a trend does not exist, create one
+      if (!trend) {
+        trend = new db.Trend({
+          politician: body.politicianId,
+          order: body.order || totalTrends + 1,
+        });
+        await trend.save();
+      }
+
+      res.status(200).send({
+        message: 'Trend created successfully.',
+        trend: OutputFormatters.formatTrend(trend)
+      });
+    } catch (error) {
+      next(new ErrorHandler(500, error.message));
+    }
+  }
+}
+
+module.exports = Trends;
