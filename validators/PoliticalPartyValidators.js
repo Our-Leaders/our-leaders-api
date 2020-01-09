7de1d8a9399fdeaac4544783f53edb27d5f8cca7
Ralph Olutola <oreofe.olutola@gmail.com>
2020-01-09 22:30:25 +0100

    Validator for edit political party

--- validators/PoliticalPartyValidators.js
+++ validators/PoliticalPartyValidators.js
@@ -23,6 +23,31 @@ class PoliticalPartyValidators {
       next();
     }
   }
+
+  static async validateUpdate(req, res, next) {
+    const { body } = req;
+    let message = '';
+
+    if (body.name !== undefined && body.name.trim().length < 1) {
+      message = 'Please enter a valid name for the political party name';
+    } else if (body.yearEstablished !== undefined && body.yearEstablished <= 0) {
+      message = 'Please enter a valid value for the political party year established';
+    } else if (body.partyLeader !== undefined && body.partyLeader.trim().length < 1) {
+      message = 'Please enter a valid value for party leader';
+    } else if (body.partyBackground !== undefined && body.partyBackground.trim().length < 1) {
+      message = 'Please enter a valid value for party background';
+    } else if (body.ideology !== undefined && body.ideology.trim().length < 1) {
+      message = 'Please enter a valid value for party ideology';
+    } else if (body.acronym !== undefined && body.acronym.trim().length < 1) {
+      message = 'Please enter a valid value for party acronym';
+    }
+
+    if (!message) {
+      next();
+    } else {
+      next(new ErrorHandler(400, message));
+    }
+  }
 }
 
 module.exports = PoliticalPartyValidators;
