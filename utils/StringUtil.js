861cb88bb77f7b37392e2ddcdbe09665a0fa7e5f
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-01-25 17:14:19 +0100

    ft: add method to generate transaction references

--- utils/StringUtil.js
+++ utils/StringUtil.js
@@ -6,6 +6,12 @@ class StringUtil {
   static isValidEmail(email) {
     return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email);
   }
+
+  static generateTransactionReference() {
+    const prefix = 'OLA';
+    const suffix = Math.random().toString().slice(0, 10);
+    return `${prefix}-${suffix}`
+  }
 }
 
 module.exports = StringUtil;
