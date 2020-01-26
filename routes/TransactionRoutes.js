8ef867f674998cd9745b5eaf91692e3ee6b311b5
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-01-26 15:13:58 +0100

    ft: add transactions webhook route

--- routes/TransactionRoutes.js
+++ routes/TransactionRoutes.js
@@ -3,4 +3,7 @@ const TransactionsCtrl = require('./../controllers/Transactions');
 module.exports = (router) => {
   router.route('transactions/initialize')
     .post(TransactionsCtrl.initialize);
+
+  router.route('transactions/webhook')
+    .post(TransactionsCtrl.webhook);
 };
