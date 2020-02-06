c0da006a730bee08b4e903d584a3f58f124b8de4
chinezenwosu <chineze.nwosu@andela.com>
2020-02-06 03:16:52 +0100

    create contrl methods for update and delete trands, fix create method issue

--- controllers/Trends.js
+++ controllers/Trends.js
@@ -7,16 +7,19 @@ class Trends {
     const {body} = req;
 
     try {
-      let totalTrends = await db.Trend.countDocuments();
+      const totalTrends = await db.Trend.countDocuments();
       let trend = await db.Trend.findOne({
         politician: body.politicianId
       });
 
       // if a trend does not exist, create one
       if (!trend) {
+        const nextOrder = totalTrends + 1;
+        await resortTrendOrder(nextOrder, body.order);
+
         trend = new db.Trend({
           politician: body.politicianId,
-          order: body.order || totalTrends + 1,
+          order: body.order || nextOrder,
         });
         await trend.save();
       }
@@ -49,7 +52,7 @@ class Trends {
   static async getTrendingPoliticians(req, res, next) {
     try {
       const trendingPoliticiansLimit = 10; // This is just an arbitrary number, can be changed later or added to future settings
-      const trends = await db.Trend.find().populate('politician');
+      const trends = await db.Trend.find().sort('order').populate('politician');
       let trendingPoliticians = [];
 
       if (trends.length < trendingPoliticiansLimit) {
@@ -75,6 +78,73 @@ class Trends {
       });
     } catch (error) {
       next(new ErrorHandler(500, error.message));
+    }
+  }
+
+  static async update(req, res, next) {
+    const {body, params} = req;
+    const {id} = params;
+    const {order} = body;
+
+    try {
+      const trend = await db.Trend.findById(id);
+      const previousOrder = trend.order;
+
+      if (!trend) {
+        return next(new ErrorHandler(404, 'Trending politician doesn\'t exist'));
+      }
+
+      await resortTrendOrder(previousOrder, order);
+      trend.order = order;
+      await trend.save();
+
+      res.status(200).send({
+        trend: OutputFormatters.formatTrend(trend)
+      });
+    } catch (error) {
+      next(new ErrorHandler(500, error.message));
+    }
+  }
+
+  static async delete(req, res, next) {
+    const {id} = req.params;
+
+    try {
+      const trend = await db.Trend.findById(id);
+      const totalTrends = await db.Trend.countDocuments();
+
+      if (trend) {
+        await db.Trend.findByIdAndDelete(id);
+      }
+      resortTrendOrder(trend.order, totalTrends + 1);
+
+      res.status(200).send({
+        message: 'Trend deleted successfully.'
+      });
+    } catch (error) {
+      next(new ErrorHandler(500, error.message));
+    }
+  }
+
+  static async deleteAll(req, res, next) {
+    try {
+      await db.Trend.deleteMany({});
+
+      res.status(200).send({
+        message: 'All trends deleted successfully.'
+      });
+    } catch (error) {
+      next(new ErrorHandler(500, error.message));
+    }
+  }
+}
+
+const resortTrendOrder = async(previousOrder, newOrder) => {
+  if (newOrder && previousOrder !== newOrder) {
+    if (previousOrder > newOrder) {
+      await db.Trend.updateMany({order: {$gte: newOrder, $lt: previousOrder}}, {$inc: {order: 1}});
+    } else {
+      await db.Trend.updateMany({order: {$gt: previousOrder, $lte: newOrder}}, {$inc: {order: -1}});
     }
   }
 }
