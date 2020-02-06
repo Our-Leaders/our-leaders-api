c9f58101dfd0563fa5317d697b70fbacc3fad66b
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-02-06 13:47:09 +0100

    ft: update politician feed retrieval logic

--- controllers/Feeds.js
+++ controllers/Feeds.js
@@ -38,26 +38,10 @@ class Feeds {
   }
 
   static async getPoliticianFeeds(req, res, next) {
-    const {user, query} = req;
-    const {politicianId} = query;
+    const {politicianId} = req.params;
 
     try {
-      let politicianIds;
-
-      // if a politician id query is set, then use that
-      if (politicianId) {
-        politicianIds = [politicianId];
-      } else {
-        // get politician ids for all user subscriptions
-        const subscriptions = await db.Subscription
-          .find({user: user.id})
-          .select('politician');
-
-        politicianIds = subscriptions.map(x => {
-          return x.politician;
-        });
-      }
-
+      const politicianIds = [politicianId];
       // check for feeds with the politician ids
       const feeds = await FeedUtil.queryFeeds(politicianIds);
 
