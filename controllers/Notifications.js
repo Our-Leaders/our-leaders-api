e6bf152cd8fccc45b0d33ce630c4ad10ae144840
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-02-25 15:43:27 +0100

    ft: finalize notifications retrieval logic

--- controllers/Notifications.js
+++ controllers/Notifications.js
@@ -8,10 +8,36 @@ const {ErrorHandler} = require('../utils/ErrorUtil');
 
 class Notifications {
   static async getAll(req, res, next) {
-    const {user} = req;
-
     try {
+      const user = await db.User.findById(req.user.id);
 
+      if (!user) {
+        return next(new ErrorHandler(404, 'Caller not found.'));
+      }
+
+      // if the user has not accessed notifications, then use the unix epoch
+      const notificationLastAccessedAt = user.notificationsLastRetrievedAt || new Date(1970, 0, 1);
+
+      const subscriptions = await db.Subscription
+        .find({
+          email: user.email
+        })
+        .select('politician');
+      const subscribedPoliticians = subscriptions.map(x => x.politician);
+
+      const notifications = await db.Notification
+        .find({
+          createdAt: {
+            $gte: notificationLastAccessedAt
+          },
+          entityId: {
+            $in: subscribedPoliticians
+          }
+        });
+
+      res.status(200).send({
+        notifications: notifications.map(x => OutputFormatters.formatNotification(x))
+      });
     } catch (error) {
       next(new ErrorHandler(500, error.message));
     }
