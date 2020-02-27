a9f40fa4d550c2575e5c18bd458820dc55130780
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-02-27 13:34:09 +0100

    ft: complete mailchimp util logic

--- utils/MailChimpUtil.js
+++ utils/MailChimpUtil.js
@@ -9,6 +9,14 @@ const mailChimp = new MailChimp(Config.mailChimp.apiKey);
 
 class MailChimpUtil {
   static async addUserToList(user) {
-
+    const listId = 'bd885c9eb2';
+    await mailChimp.post(`/lists/${listId}/members`, {
+      email_address: user.email,
+      email_type: 'html',
+      status: 'subscribed',
+      timestamp_signup: (new Date()).toISOString()
+    });
   }
 }
+
+module.exports = MailChimpUtil;
