e4d471a1383a6fa45c0d3b4b1c40ef91bc95c796
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-03 09:57:29 +0100

    ch: cleanup

--- config/Config.js
+++ config/Config.js
@@ -28,11 +28,6 @@ module.exports = {
     cloudName: process.env.CLOUDINARY_CLOUD_NAME,
     secret: process.env.CLOUDINARY_API_SECRET
   },
-  sms: {
-    baseUrl: process.env.SMS_BASE_URL,
-    bsApiKey: process.env.BS_API_KEY,
-    bsSenderId: process.env.BS_SENDER_ID
-  },
   feedUrl: 'https://www.tostvnetwork.com/category/news/',
   superAdmin: {
     email: process.env.SUPER_EMAIL,
