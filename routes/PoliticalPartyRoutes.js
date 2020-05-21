b2b493d6d2f7f75d4f425d23e33c13399c4c9cd3
Oreofe Olutola <oreofe.olutola@gmail.com>
2020-05-21 22:25:02 +0100

    Fix wrong route placement for deleting parties

--- routes/PoliticalPartyRoutes.js
+++ routes/PoliticalPartyRoutes.js
@@ -19,12 +19,7 @@ module.exports = (router) => {
       ImageMiddleware.uploadLogo,
       PoliticalPartyValidators.validateCreation,
       PoliticalPartyCtrl.create
-    )
-    .delete(
-      AuthMiddleware.authenticate,
-      AuthMiddleware.isAdmin,
-      PoliticalPartyCtrl.delete
-    )
+    );
 
   router.route('/political-party/:id')
     .get(PoliticalPartyCtrl.get)
@@ -34,6 +29,11 @@ module.exports = (router) => {
       ImageMiddleware.uploadLogo,
       PoliticalPartyValidators.validateUpdate,
       PoliticalPartyCtrl.edit
+    )
+    .delete(
+      AuthMiddleware.authenticate,
+      AuthMiddleware.isAdmin,
+      PoliticalPartyCtrl.delete
     );
 
   router.route('/political-party/:id/vote')
