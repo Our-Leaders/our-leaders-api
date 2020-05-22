291f18fe8dc8ff44748b8a130ab0c5316fd57df5
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-22 07:35:13 +0100

    ft: add statistic model

---
+++ models/Statistic.js
@@ -0,0 +1,28 @@
+/**
+ * Created by bolor on 5/22/2020
+ */
+
+const shortId = require('shortid');
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const StatisticModel = mongoose.model('Statistic', new Schema({
+  _id: {
+    type: String,
+    default: shortId.generate
+  },
+  referrer: {
+    type: String
+  },
+  pageUrl: {
+    type: String
+  },
+  userIp: {
+    type: String
+  }
+}, {
+  timestamps: true
+}));
+
+module.exports = StatisticModel;
