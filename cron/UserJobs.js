40fb78d68b96aafc201220bc1fe619d30fc6e957
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-26 18:28:26 +0100

    ft: add user job scaffold

--- cron/UserJobs.js
+++ cron/UserJobs.js
@@ -6,7 +6,31 @@ const db = require('./../models');
 
 class UserJobs {
   static async run() {
+    const thirtyDaysAgo = new Date();
+    thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
 
+    const eligibleUsers = await db.User
+      .find({
+        isDeleted: true,
+        lastActiveAt: {
+          $lte: thirtyDaysAgo
+        }
+      })
+      .select('_id email');
+
+    for (let eligibleUser of eligibleUsers) {
+      // remove the user subscriptions
+      await db.Subscription
+        .deleteMany({
+          email: eligibleUser.email
+        });
+
+      // remove user account
+      await db.User
+        .deleteOne({
+          _id: eligibleUser._id
+        });
+    }
   }
 }
 
