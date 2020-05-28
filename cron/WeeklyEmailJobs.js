44dafccfd42591592edcf04b41a6eeb6e800125a
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-28 14:28:59 +0100

    ft: pull politician notifications

--- cron/WeeklyEmailJobs.js
+++ cron/WeeklyEmailJobs.js
@@ -2,9 +2,40 @@
  * Created by bolor on 5/28/2020
  */
 
+const _ = require('lodash');
+const db = require('./../models');
+const Logger = require('./../config/Logger');
+
 class WeeklyEmailJobs {
   static async run() {
+    try {
+      const endDate = new Date();
+      const startDate = new Date(endDate.getDate() - 7);
 
+      const subscriptions = await db.Subscription
+        .find({
+          type: 'email'
+        })
+        .select('politician');
+      const politicianIds = _.uniq(subscriptions
+        .map(x => x.politician));
+
+      const politicianNotificationMap = {};
+      for (let politicianId of politicianIds) {
+        politicianNotificationMap[politicianId] = await db.Notification
+          .findOne({
+            entityType: 'politician',
+            entityId: politicianId,
+            createdAt: {
+              $gte: startDate,
+              $lte: endDate
+            }
+          });
+      }
+    } catch (err) {
+      Logger.error(err);
+      Logger.error('An error occurred when refreshing feeds.')
+    }
   }
 }
 
