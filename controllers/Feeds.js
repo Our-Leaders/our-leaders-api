5e3b566367de4728f34a50ddd6242c80361769e8
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-01 13:51:16 +0100

    ft: add in the feeds controller scaffold

--- controllers/Feeds.js
+++ controllers/Feeds.js
@@ -8,6 +8,35 @@ const OutputFormatters = require('./../utils/OutputFormatters');
 const FeedUtil = require('./../utils/FeedUtil');
 
 class Feeds {
+  static async getFeeds(req, res, next) {
+    const {email} = req.user;
+
+    try {
+      // get politician ids for all user feed subscriptions
+      const subscriptions = await db.Subscription
+        .find({
+          email,
+          type: 'feeds'
+        })
+        .select('politician');
+
+      const politicianIds = subscriptions.map(x => {
+        return x.politician;
+      });
+
+      // check for feeds with the politician ids
+      const feeds = await FeedUtil.queryFeeds(politicianIds);
+
+      res.status(200).send({
+        feeds: feeds.map(x => {
+          return OutputFormatters.formatFeed(x)
+        })
+      });
+    } catch (error) {
+      next(new ErrorHandler(500, error.message));
+    }
+  }
+
   static async getUserFeeds(req, res, next) {
     const {email} = req.user;
 
