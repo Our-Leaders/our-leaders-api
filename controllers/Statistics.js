94d90664a1c10080a2fa5580008fa9427807e6be
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-10 14:09:14 +0100

    ft: add geocoding when registring visits

--- controllers/Statistics.js
+++ controllers/Statistics.js
@@ -7,6 +7,7 @@ const moment = require('moment');
 const db = require('./../models');
 const {ErrorHandler} = require('../utils/ErrorUtil');
 const OutputFormatters = require('./../utils/OutputFormatters');
+const GeoCodingUtil = require('./../utils/GeoCodingUtils');
 
 class Statistics {
   static async getStats(req, res, next) {
@@ -52,7 +53,7 @@ class Statistics {
     const result = {};
 
     if (startDate && endDate) {
-      dayDiff = ((new Date(endDate).getTime() - new Date(startDate).getTime())/(1000 * 60 * 60 * 24)) + 1;
+      dayDiff = ((new Date(endDate).getTime() - new Date(startDate).getTime()) / (1000 * 60 * 60 * 24)) + 1;
     }
 
     try {
@@ -95,7 +96,7 @@ class Statistics {
 
       const formattedVisits = visitsByDate.map(x => OutputFormatters.formatVisitStats(x));
       formattedVisits.forEach(x => result[x.date] = x.visits);
-      const payload = Object.keys(result).map(x => ({ date: new Date(x), visits: result[x] }))
+      const payload = Object.keys(result).map(x => ({date: new Date(x), visits: result[x]}))
 
       res.status(200).send({
         visits: payload
@@ -111,7 +112,7 @@ class Statistics {
     const result = {};
 
     if (startDate && endDate) {
-      dayDiff = ((new Date(endDate).getTime() - new Date(startDate).getTime())/(1000 * 60 * 60 * 24)) + 1;
+      dayDiff = ((new Date(endDate).getTime() - new Date(startDate).getTime()) / (1000 * 60 * 60 * 24)) + 1;
     }
 
     try {
@@ -129,7 +130,7 @@ class Statistics {
           $lte: new Date(endDate)
         };
       }
-      
+
       for (let i = 0; i < dayDiff; i++) {
         const day = new Date(new Date(startDate).getTime() + (i * 24 * 60 * 60 * 1000));
         result[day] = 0;
@@ -154,7 +155,8 @@ class Statistics {
 
       const formattedSignups = signUpsByDate.map(x => OutputFormatters.formatSignupStats(x))
       formattedSignups.forEach(x => result[x.date] = x.signUps);
-      const payload = Object.keys(result).map(x => ({ date: new Date(x), signUps: result[x] }));
+      const payload = Object.keys(result)
+        .map(x => ({date: new Date(x), signUps: result[x]}));
 
       res.status(200).send({
         signUps: payload
@@ -173,10 +175,12 @@ class Statistics {
         return res.status(200).send({});
       }
 
+      const location = await GeoCodingUtil.getLocationFromIp(req.clientIp);
       const statistic = new db.Statistics({
         referrer,
         pageUrl: url,
-        userIp: req.clientIp
+        userIp: req.clientIp,
+        origin: location
       });
       await statistic.save();
 
@@ -243,7 +247,7 @@ class Statistics {
           $lte: new Date(endDate)
         };
       }
-      
+
       for (let i = 0; i < dayDiff; i++) {
         const day = moment(startDate).add(i, 'day').toDate();
         result[day] = 0;
@@ -265,22 +269,22 @@ class Statistics {
               // donations: {$sum: '$amount' }
               // doing this because paystack always charges using the kobo/cent value
               // rather than naira/dollar
-              donations: {$sum: { $divide: ['$amount', 100] } }
+              donations: {$sum: {$divide: ['$amount', 100]}}
             }
           },
           {
             $group: {
-                _id: {
-                    'currency': '$_id.currency'
-                },
-                data: {
-                    $push: {
-                        donations: '$donations',
-                        'year': '$_id.year',
-                        'month': '$_id.month',
-                        'day': '$_id.day',
-                    }
+              _id: {
+                'currency': '$_id.currency'
+              },
+              data: {
+                $push: {
+                  donations: '$donations',
+                  'year': '$_id.year',
+                  'month': '$_id.month',
+                  'day': '$_id.day',
                 }
+              }
             }
           },
           {
@@ -293,7 +297,7 @@ class Statistics {
                   as: "result",
                   in: {
                     $mergeObjects: [
-                      { donations: '$$result.donations' },
+                      {donations: '$$result.donations'},
                       {
                         date: {
                           $dateFromParts: {
@@ -314,12 +318,12 @@ class Statistics {
 
       // fill in the empty dates with zeros
       const payload = aggregateResult.map(datum => {
-        const resultToModify = { ...result };
+        const resultToModify = {...result};
         datum.data.forEach(p => {
           resultToModify[p.date] = p.donations
         });
 
-        const dataToReturn = Object.keys(resultToModify).map(a => ({ date: new Date(a), donations: resultToModify[a] }))
+        const dataToReturn = Object.keys(resultToModify).map(a => ({date: new Date(a), donations: resultToModify[a]}))
         return {
           currency: datum.currency,
           data: dataToReturn,
