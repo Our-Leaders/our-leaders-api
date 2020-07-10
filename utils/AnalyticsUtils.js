c8d94ea866f669082e2a4d9ff3477b5592b85942
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-10 16:35:09 +0100

    ft: update location analytics call

--- utils/AnalyticsUtils.js
+++ utils/AnalyticsUtils.js
@@ -2,11 +2,47 @@
  * Created by bolor on 7/10/2020
  */
 
+const moment = require('moment');
+
 class AnalyticsUtils {
-  static async getLocationAnalytics(optionalQuery = null) {
+  static async getLocationAnalytics(limit = null, date = null) {
     const pipeline = [
       {$group: {_id: '$origin', visitors: {$sum: 1}}},
       {$sort: {visitors: -1}}
+    ];
+
+    if (limit) {
+      pipeline.push({$limit: limit});
+    }
+
+    if (date) {
+      const day = moment(date);
+      const start = day.startOf('day').toDate();
+      const end = day.endOf('day').toDate();
+      pipeline.unshift({
+        timestamp: {
+          $gte: start,
+          $lte: end
+        }
+      });
+    }
+
+    const results = await db.Statistics
+      .aggregate(pipeline);
+
+    return results.map((x, index) => {
+      return {
+        name: x._id,
+        rank: index + 1,
+        visits: x.visits
+      }
+    });
+  }
+
+  static async getPageViewAnalytics(optionalQuery = null) {
+    const pipeline = [
+      {$group: {_id: '$pageUrl', viewCount: {$sum: 1}}},
+      {$sort: {viewCount: -1}}
     ];
 
     if (optionalQuery) {
@@ -18,9 +54,10 @@ class AnalyticsUtils {
 
     return results.map((x, index) => {
       return {
-        name: x._id,
+        name: x.pageTitle,
+        url: x._id,
         rank: index + 1,
-        visits: x.visits
+        viewCount: x.viewCount
       }
     });
   }
